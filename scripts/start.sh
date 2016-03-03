7817ccec88215961be1abd4abee2d250bc5eb5ee
Khaliq Gant <khaliqgant@gmail.com>
2016-03-02 20:22:06 -0500

    add in self documenting shell script example

---
+++ scripts/start.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+##
+## Usage: sh start.sh
+## Example self documenting help bash script
+##
+
+prog="$0"
+me=`basename "$prog"`
+
+RED='\033[0;31m'
+
+help () {
+  grep '^##' "$prog" | sed -e 's/^##//' -e "s/_PROG_/$me/" 1>&2
+}
+
+##
+## Usage: sh start.sh method
+## Another method that self documents!
+##
+method() {
+    echo "method"
+    echo $1
+}
+
+##
+## Usage: sh start.sh another
+## Another one!
+##
+another() {
+    echo "another one! $1"
+}
+
+# this calls the function passed as the first paramter to the script
+# the $2 makes the next arg available passed to the method
+$1 $2
